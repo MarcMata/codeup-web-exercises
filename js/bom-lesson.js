ab24d61814f9bd4d23489930bf46186014598d93
Marc Mata <marcmata26@gmail.com>
2023-03-13 13:31:02 -0500

    dom lesson and exercise complete. Bom lesson started

---
+++ js/bom-lesson.js
@@ -0,0 +1,22 @@
+'use strict';
+//produce console log every second
+let count = 0;
+let max = 10;
+let interval = 1000; //interval time in milliseconds
+
+// let intervalId = setInterval(function(){
+//     if (count >= max) {
+//         clearInterval(intervalId)
+//         console.log('All done!');
+//     } else {
+//         count++;
+//         console.log('Repeating this line ' + count + ' times');
+//     }
+// }, interval)
+
+// console.log('5 seconds starts now!');
+// setTimeout(function(){
+//     console.log(`It's been 5 seconds!`);
+// }, 5000);
+
+window.location.href = 'https://google.com';
