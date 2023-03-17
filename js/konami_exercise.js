62ba0cfe989b15f12a4515be208d0b3aeb56b666
Marc Mata <marcmata26@gmail.com>
2023-03-17 16:14:19 -0500

    intro to jquery, jquery exercises, Konami exercise complete.

--- js/konami_exercise.js
+++ js/konami_exercise.js
@@ -1,10 +1,13 @@
 "use strict";
 
-$('.page-wrapper').css('background-color','black');
+$('.page-wrapper').css({
+    'background-color' : '#C72331',
+});
 $('h1').css('color', 'white')
 
 let cheatCode = [38, 38, 40 ,40,37,39,37,39,66, 65, 13]
 let arrayIndex = 0;
+let audioElement = $("#my-audio")[0];
 
 $(document).keyup(function(event){
     console.log(event.keyCode);
@@ -19,8 +22,33 @@ $(document).keyup(function(event){
     if (arrayIndex === cheatCode.length) {
         alert('Konami code entered!');
         arrayIndex = 0;
-        $('.page-wrapper').css('background-color','green')
+        $('.page-wrapper').css({
+            'background-color' : 'white',
+            'background-image' : 'url("../images/lost_art_cards.png")',
+            'background-repeat': 'no-repeat',
+            'background-size' : 'cover',
+        });
+        $('h1').css({
+            'color': 'red',
+        });
+        if (audioElement.paused) {
+            audioElement.play();
+            $(this).text("Pause Sound");
+        } else {
+            audioElement.pause();
+            $(this).text("Play Sound");
+        }
     }
 });
-// 38, 38, 40 ,40,37,39,37,39,66, 65
 
+// var audioElement = $("#my-audio")[0];
+// $("#play-pause-sound").click(function () {
+//     if (audioElement.paused) {
+//         audioElement.play();
+//         $(this).text("Pause Sound");
+//     } else {
+//         audioElement.pause();
+//         $(this).text("Play Sound");
+//     }
+// });
+// 38, 38, 40 ,40,37,39,37,39,66, 65
