571418df0b4eeb3cd0745f699c3b745f251d01a0
Marc Mata <marcmata26@gmail.com>
2023-03-22 11:01:50 -0500

    completed ajax blog exercise

---
+++ js/ajax-blog.js
@@ -0,0 +1,22 @@
+const getBlogs = async (posts) => {
+    let blogPosts = document.querySelector("#posts")
+
+    // blogPosts.innerHTML = ``
+    let response = await fetch(`data/blog.json`)
+    let returnedPosts = await response.json();
+    returnedPosts.forEach(function(e){
+        let row = document.createElement('div')
+        row.innerHTML = `
+                <h3>${e.title}</h3>
+                <p>${e.content}</p>
+                <p>Categories: ${e.categories}</p>
+                <p>Posted: ${e.date}</p>
+            `;
+        blogPosts.appendChild(row)
+    });
+
+}
+
+(async()=>{
+    let posts = await getBlogs();
+})();
