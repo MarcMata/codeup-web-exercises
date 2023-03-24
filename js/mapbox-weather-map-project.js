c39f5195ebf86cf75c03a6d61c0d5318d0f72b43
Marc Mata <marcmata26@gmail.com>
2023-03-24 16:14:47 -0500

    Weather map revised and functions working--need to add click to add marker

--- js/mapbox-weather-map-project.js
+++ js/mapbox-weather-map-project.js
@@ -1,3 +1,12 @@
+function debounce(func, delay) {
+    let timer;
+    return function(...args) {
+        const context = this;
+        clearTimeout(timer);
+        timer = setTimeout(() => func.apply(context, args), delay);
+    };
+}
+
 document.querySelector("#zoomSubmit").addEventListener('click', event => {
     event.preventDefault();
     map.setZoom(document.querySelector("#zoom").value);
@@ -26,52 +35,12 @@ let setMarkerButton = document.querySelector("#setMarkerButton")
         console.log(markerCoords[1])
         setMarkerButton.addEventListener('click', event=>{
            newMarker.remove();
-
         });
+        weatherMapDaily(markerCoords[0],markerCoords[1])
+        weatherMap5Day3Hour(markerCoords[0], markerCoords[1])
     });
 });
 
-
-const restarauntsArr = [
-    {
-        name: "Taco Palenque",
-        about: "Taco Palenque is a Mexican cuisine restaurant chain in South Texas and northern Tamaulipas, Mexico. The restaurant is headquartered in Laredo, Texas and was established in 1987.",
-        address: "1002 NE Interstate 410 Loop, San Antonio, TX 78209",
-        LngLat: [-98.459361, 29.515619]
-    },
-    {
-        name: "Whataburger",
-        about: "Whataburger is an American regional fast food restaurant chain, headquartered and based in San Antonio, Texas, that specializes in hamburgers. The company, founded by Harmon Dobson and Paul Burton, opened its first restaurant in Corpus Christi, Texas, in 1950.",
-        address: "412 E Commerce St, San Antonio, TX 78205",
-        LngLat:[-98.488625, 29.423796]
-    },
-    {
-        name: "La Colmena",
-        about: "La Colmena is a restaraunt owned by two brothers started around 2019. They serve fruit cups, elote, burgers, hot dogs, and even tamales. They have an assortment of fruit-related drinks.",
-        address: "15171 Judson Rd #105, San Antonio, TX 78259",
-        LngLat:[-98.37377, 29.57976]
-    },
-]
-
-restarauntsArr.forEach((e) => {
-    const element = document.createElement('div');
-    element.className = 'marker';
-    const restaurants = new mapboxgl.Marker(element)
-        .setLngLat(e.LngLat)
-        .addTo(map)
-    const popup = new mapboxgl.Popup()
-        .setHTML(`
-                <h4>${e.name}</h4>
-                <p class="popup">${e.about}</p>
-                `
-        );
-    restaurants.setPopup(popup);
-
-    // document.querySelector("#noMoreMarkersButton").addEventListener('click',event=>{
-    //     event.preventDefault();
-    //     restaurants.remove();
-    // });
-});
 document.querySelector("#noMoreMarkersButton").addEventListener('click',event=> {
     event.preventDefault();
     document.querySelectorAll(".mapboxgl-marker").forEach(svg=>{
--- js/weather-map-project.js
+++ js/weather-map-project.js
@@ -1,62 +1,109 @@
 //https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid={API key}
 const SALng = -98.48527;
 const SALat = 29.423017;
-let newMarkerLng = markerCoords[0]
-let newMarkerLat = markerCoords[1]
 
-async function weatherMapDaily(){
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather/?lat=${SALat}&lon=${SALng}&units=imperial&appid=${OPEN_WEATHER_APPID}`);
-    const data =  await response.json();
+
+//adds todays forecast
+let addingTodaysWeather = document.querySelector('.today-weather-card')
+
+
+async function weatherMapDaily(lat = 29.423017, long= -98.48527) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather/?lat=${lat}&lon=${long}&units=imperial&appid=${OPEN_WEATHER_APPID}`);
+    const data = await response.json();
     console.log(data);
-    console.log('****************************************')
-    console.log(`Today's current temperature: ${data.main.temp}`)
-    console.log(`Today's Max Temp: ${data.main.temp_max}`)
-    console.log(`Today's Min Temp: ${data.main.temp_min}`)
-    console.log(`Today's weather description: ${data.weather[0].description}`)
-    console.log(`Today's wind degree: ${data.wind.deg}`)
-    console.log(`Today's wind speed: ${data.wind.speed}`)
-    console.log('****************************************')
-    // data.forEach((e,i) =>{
-    //
-    // });
+    addingTodaysWeather.innerHTML = `
+                                <h3 class="fontSizeLarge today-title">${data.name}</h3>
+                                <p class="fontSizeLarge weather-card-text">${Math.round(data.main.temp)}°</p>
+                                <img class="today-icon" src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="${data.weather[0].description}">
+                                <p class="fontSizeMed weather-card-text">${data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1)}</p>
+                                <p class="fontSizeMed weather-card-text">High: ${Math.round(data.main.temp_min)}°</p>
+                                <p class="fontSizeMed weather-card-text">Low: ${Math.round(data.main.temp_max)}°</p>
+                                <p class="fontSizeMed weather-card-text">Wind speed: ${data.wind.speed}mph</p>
+                                <p class="fontSizeMed weather-card-text" >Wind direction: ${data.wind.deg}°</p>
+                                `;
 }
-(async()=>{
+
+(async () => {
     let dailyWeather = await weatherMapDaily();
 })();
 
+
+//Adds four day forecast cards
 daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 const addingForecastCards = document.querySelector("#forecastDiv")
-async function weatherMap5Day3Hour(){
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${SALat}&lon=${SALng}&units=imperial&appid=${OPEN_WEATHER_APPID}`);
-    const data =  await response.json();
+
+async function weatherMap5Day3Hour(lat = 29.423017, long= -98.48527) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=imperial&appid=${OPEN_WEATHER_APPID}`);
+    const data = await response.json();
     console.log(data);
     let forecastHTML = '';
-    data.list.forEach((e,i) =>{
-        if (i % 8 === 0 && i !== 0){
-            const time = new Date(e.dt*1000);
-            // console.log(time.getHours());
-            // console.log(time.getTime());
-            //  <div class="column weatherCard">
-            //                         <h3>Monday</h3>
-            //                     </div>
+    data.list.forEach((e, i) => {
+        if (i % 8 === 0 && i !== 0) {
+            const time = new Date(e.dt * 1000);
             forecastHTML += `
-                    <div class="column weatherCard align-center ">
+                    <div class="column weather-card align-center">
                         <h3>${daysOfTheWeek[time.getDay()]}</h3>
-                        <p>${e.weather[0].description.toUpperCase()}</p>
+                        <p>${e.weather[0].description.charAt(0).toUpperCase() + e.weather[0].description.slice(1)}</p>
                         <img class="icon" src="https://openweathermap.org/img/w/${e.weather[0].icon}.png" alt="${e.weather[0].description}">
                         <p>High: ${e.main.temp_min}°</p>
                         <p>Low: ${e.main.temp_max}°</p>
                     </div>
             `;
             console.log(daysOfTheWeek[time.getDay()]);
-            // console.log(e.dt_txt);
-            // console.log(e.weather[0].description)
-            // console.log(e.main.temp_min)
-            // console.log(e.main)
         }
     });
     addingForecastCards.innerHTML = forecastHTML;
 }
-(async()=>{
+
+(async () => {
     let threeHourWeather = await weatherMap5Day3Hour();
 })();
+
+//months of the year
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+//Grabs the date time
+
+const timeElement = document.querySelector('#time');
+const dateElement = document.querySelector('#date');
+const currentWeatherElement = document.querySelector('#current-weather');
+const timezone = document.querySelector('#time-zone');
+const currentTemp = document.querySelector('#current-temp')
+
+
+// ACTIVATE WHEN COMPLETE
+setInterval(() => {
+   const time = new Date();
+   const month = time.getMonth();
+   const date = time.getDate();
+   const day = time.getDay();
+   const hour = time.getHours();
+   const hoursIn12HrFormat = hour >= 13 ? hour % 12: hour
+   let minutes = `${time.getMinutes()}`.length === 1 ? '0' + time.getMinutes() : time.getMinutes();
+   const ampm = hour >= 12 ? 'PM' : 'AM'
+
+    timeElement.innerHTML = `
+                            <div class="column align-center" >
+                                <h4 class="fontSizeXLarge white-text">${hoursIn12HrFormat}:${minutes}<span class="fontSizeMed white-text">${ampm}</span></h4>
+                            </div>
+                            `;
+    dateElement.innerHTML = `
+                            <div class="column align-center">
+                                <h4 class="fontSizeMed white-text">${daysOfTheWeek[day]}, ${date} ${months[month]}</h4>
+                            </div>
+                            `;
+},500);
+
+
+//weather forecast for every three hours for forty instances
+// const daysEnd = [];
+// data.list.forEach((forecast, index) => {
+//     const dateTime = new Date (forecast.dt * 1000);
+//     if (dateTime.getHours() === 22)
+//         daysEnd.push(index);
+// });
+// console.log(daysEnd)
+//
+// daysEnd.forEach((e,i){
+//     const temperatures = []
+//     temperatures.push(e.list[i].main.temp);
+// });
